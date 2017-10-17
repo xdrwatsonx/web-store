6072a64116a4f423ce4d59c82ba23d4f41f7ecd7
xdrwatsonx <lider_m@mail.ru>
2017-10-17 22:25:19 +0300

    мелкие дороботки по страницам, вставлен Slicknav, адаптация индекса до 767, выполнены все задания и правки по состоянию на 14.10.17

--- app/js/init.js
+++ app/js/init.js
@@ -75,4 +75,10 @@ $( function() {
     // formstyler init
     $('.input-quantity').styler();
 
+    // slick nav init
+    $('.header-nav').slicknav({
+        appendTo: 'header .bottom-header .flex-container',
+        label: ''
+    });
+
 } );
